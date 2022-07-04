26f69341d262f602c6730c3c3254fd10c806e544
Nikita Sheverdyaev <nikita.sheverdyaev@yandex.ru>
2022-07-04 18:43:10 +0300

    perf: Add blank fragment
    It is basis for single-activity architecture.

--- app/src/main/java/com/example/wowpersonalitytest/MainActivity.kt
+++ app/src/main/java/com/example/wowpersonalitytest/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.wowpersonalitytest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import com.example.wowpersonalitytest.fragments.QuestionFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container_view, QuestionFragment())
+            .commit()
     }
- f
+
 }
