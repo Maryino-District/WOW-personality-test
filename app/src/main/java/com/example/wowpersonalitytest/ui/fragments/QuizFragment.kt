a1f143592dc20a83def05c4c522a6afbc50c8432
Nikita Sheverdyaev <nikita.sheverdyaev@yandex.ru>
2022-07-16 09:59:17 +0300

    style: Add resources
    Add new images with bettter resolution and questions of quiz.

--- app/src/main/java/com/example/wowpersonalitytest/ui/fragments/QuizFragment.kt
+++ app/src/main/java/com/example/wowpersonalitytest/ui/fragments/QuizFragment.kt
@@ -1,17 +1,23 @@
 package com.example.wowpersonalitytest.ui.fragments
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.wowpersonalitytest.databinding.FragmentQuestionBinding
+import com.example.wowpersonalitytest.R
+import com.example.wowpersonalitytest.data.Question
+import com.example.wowpersonalitytest.databinding.FragmentQuizBinding
+import java.lang.ref.WeakReference
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-private lateinit var fragmentBinding: FragmentQuestionBinding
+private lateinit var fragmentBinding: FragmentQuizBinding
+private lateinit var data: List<Question>
 
 /**ddd
  * A simple [Fragment] subclass.
@@ -37,8 +43,10 @@ class QuestionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        fragmentBinding = FragmentQuestionBinding.inflate(layoutInflater)
+        fragmentBinding = FragmentQuizBinding.inflate(layoutInflater)
         initListeners()
+        initTempData()
+        fragmentBinding.textView.setText(data.first().questionResId)
         return fragmentBinding.root
 
         //return inflater.inflate(R.layout.fragment_question, container, false)
@@ -46,11 +54,14 @@ class QuestionFragment : Fragment() {
 
     private fun initListeners() {
         fragmentBinding.buttonFirstAnswer.setOnClickListener(View.OnClickListener {
-            // TODO: implement
+            Toast.makeText(context, "ok, it's you", Toast.LENGTH_SHORT).show()
         })
 
         fragmentBinding.buttonSecondAnswer.setOnClickListener(View.OnClickListener {
-            // TODO: implement
+            Toast.makeText(context, "ok, it's not you", Toast.LENGTH_SHORT).apply {
+                setGravity(Gravity.TOP,0,0)
+                show()
+            }
         })
 
         fragmentBinding.buttonNextQuestion.setOnClickListener(View.OnClickListener {
@@ -60,6 +71,11 @@ class QuestionFragment : Fragment() {
         fragmentBinding.buttonPreviousQuestion.setOnClickListener(View.OnClickListener {
             // TODO: implement
         })
+
+    }
+
+    fun initTempData() {
+        data = listOf(Question(R.string.default_question, true))
 
     }
 
